e8bdc12cd6a6c3f288a9d9b5e19ee522872b0ede
Vladimir N. <lismiroslav@yandex.ru>
2025-06-13 03:03:29 +0300

    finished visual part of header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {LogoImageComponent} from './logo-image/logo-image.component';
 import {SearchComponent} from './search/search.component';
 import {ProfilePhotoComponent} from '../profile-photo/profile-photo.component';
+import {NavigationComponent} from "./navigation/navigation.component";
 
 @Component({
   selector: 'app-header',
   imports: [
     LogoImageComponent,
     SearchComponent,
-    ProfilePhotoComponent
+    ProfilePhotoComponent,
+    NavigationComponent,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
