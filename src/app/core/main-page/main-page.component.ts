385deb98492dcc38ab12912d3bb7f50d45ad55c9
Vladimir N. <lismiroslav@yandex.ru>
2025-06-13 06:23:14 +0300

    made send post block

--- src/app/core/main-page/main-page.component.ts
+++ src/app/core/main-page/main-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {ProfileInfoComponent} from './profile-info/profile-info.component';
 import {ProfileNavigationComponent} from './profile-navigation/profile-navigation.component';
+import {SendPostPlaceComponent} from './send-post-place/send-post-place.component';
 
 @Component({
   selector: 'app-main-page',
   imports: [
     ProfileInfoComponent,
-    ProfileNavigationComponent
+    ProfileNavigationComponent,
+    SendPostPlaceComponent
   ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
---
+++ src/app/core/main-page/send-post-place/send-post-place.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ProfilePhotoComponent } from '../../profile-photo/profile-photo.component';
+
+@Component({
+  selector: 'app-send-post-place',
+  standalone: true,
+  imports: [ProfilePhotoComponent],
+  templateUrl: './send-post-place.component.html',
+  styleUrls: ['./send-post-place.component.scss']
+})
+export class SendPostPlaceComponent {
+
+  onFileSelect(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (!input.files?.length) return;
+    const file = input.files[0];
+    console.log('Выбран файл:', file);
+    // Здесь можно отправить на сервер
+  }
+}
