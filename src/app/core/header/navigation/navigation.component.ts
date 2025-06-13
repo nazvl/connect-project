7b73b701e74602601ebff7f0f2dd0d610f9332dc
Vladimir N. <lismiroslav@yandex.ru>
2025-06-14 00:22:27 +0300

    made basic navigation between main and friends pages

--- src/app/core/header/navigation/navigation.component.ts
+++ src/app/core/header/navigation/navigation.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+// import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   imports: [
-    RouterLink
+    // RouterLink
   ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
