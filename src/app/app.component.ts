f17548ac2f1cb94aed676dff5e84b3ddce9f24dd
Vladimir N. <lismiroslav@yandex.ru>
2025-06-13 04:54:53 +0300

    made profile description for main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './core/header/header.component';
+import {MainPageComponent} from './core/main-page/main-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
