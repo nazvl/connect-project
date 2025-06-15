48654b33b265796ca19063d6fa26e5ab8efbfe7e
Vladimir N. <lismiroslav@yandex.ru>
2025-06-15 20:43:28 +0300

    added counter of likes (mock) and refactored profile-photo.component.ts

---
+++ src/app/core/messages-page/messages-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messages-page',
+  imports: [],
+  templateUrl: './messages-page.component.html',
+  styleUrl: './messages-page.component.scss'
+})
+export class MessagesPageComponent {
+
+}
