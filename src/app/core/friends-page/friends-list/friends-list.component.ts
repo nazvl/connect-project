80a266c10f7daae8f6e01fae9424970439184225
Vladimir N. <lismiroslav@yandex.ru>
2025-06-15 22:04:25 +0300

    made user cards in friendlist

--- src/app/core/friends-page/friends-list/friends-list.component.ts
+++ src/app/core/friends-page/friends-list/friends-list.component.ts
@@ -1,11 +1,97 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgForOf} from '@angular/common';
+import {FriendCardComponent} from './friend-card/friend-card.component';
+
+interface Friend {
+  id: number;
+  name: string;
+  profilePhoto: string;
+  job: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-friends-list',
-  imports: [],
+  imports: [
+    NgForOf,
+    FriendCardComponent
+  ],
   templateUrl: './friends-list.component.html',
   styleUrl: './friends-list.component.scss'
 })
+
 export class FriendsListComponent {
+  friends: Friend[] = [
+    {
+      id: 0,
+      name: 'Ivan Petrov',
+      profilePhoto: 'https://avatars.githubusercontent.com/u/1?v=4',
+      job: 'UI/UX Designer',
+      link: '#'
+    },
+    {
+      id: 1,
+      name: 'Petr Ivanov',
+      profilePhoto: 'https://avatars.githubusercontent.com/u/2?v=4',
+      job: 'Frontend Developer',
+      link: '#'
+    },
+    {
+      id: 2,
+      name: 'Ivanoid Gratis',
+      profilePhoto: 'https://avatars.githubusercontent.com/u/3?v=4',
+      job: 'Backend Engineer',
+      link: '#'
+    },
+    {
+      id: 3,
+      name: 'Anna Smirnova',
+      profilePhoto: 'https://randomuser.me/api/portraits/women/44.jpg',
+      job: 'Project Manager',
+      link: '#'
+    },
+    {
+      id: 4,
+      name: 'Dmitry Volkov',
+      profilePhoto: 'https://randomuser.me/api/portraits/men/45.jpg',
+      job: 'DevOps Engineer',
+      link: '#'
+    },
+    {
+      id: 5,
+      name: 'Maria Kuznetsova',
+      profilePhoto: 'https://randomuser.me/api/portraits/women/47.jpg',
+      job: 'QA Tester',
+      link: '#'
+    },
+    {
+      id: 6,
+      name: 'Sergey Nikolaev',
+      profilePhoto: 'https://randomuser.me/api/portraits/men/49.jpg',
+      job: 'Fullstack Developer',
+      link: '#'
+    },
+    {
+      id: 7,
+      name: 'Elena Vasilieva',
+      profilePhoto: 'https://randomuser.me/api/portraits/women/50.jpg',
+      job: 'Product Owner',
+      link: '#'
+    },
+    {
+      id: 8,
+      name: 'Alexey Lebedev',
+      profilePhoto: 'https://avatars.githubusercontent.com/u/4?v=4',
+      job: 'Game Developer',
+      link: '#'
+    },
+    {
+      id: 9,
+      name: 'Natalia Orlova',
+      profilePhoto: 'https://randomuser.me/api/portraits/women/51.jpg',
+      job: 'Business Analyst',
+      link: '#'
+    }
+  ];
 
 }
--- src/app/core/main-page/main-page.component.ts
+++ src/app/core/main-page/main-page.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import {ProfileInfoComponent} from './profile-info/profile-info.component';
 import {ProfileNavigationComponent} from '../profile-navigation/profile-navigation.component';
-// import {SendPostPlaceComponent} from '../send-post-page/send-post-place/send-post-place.component';
-// import {SendPostPageComponent} from '../send-post-page/send-post-page.component';
-// import {FriendsPageComponent} from '../friends-page/friends-page.component';
 import {RouterOutlet} from '@angular/router';
 
 @Component({
